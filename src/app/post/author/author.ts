0e61ae675eb0a81ed69325700a2fecbfbabc1b01
Yann-thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2025-06-18 11:11:44 +0200

    Balise image

--- src/app/post/author/author.ts
+++ src/app/post/author/author.ts
@@ -2,14 +2,14 @@ import { Component, inject } from '@angular/core';
 import { Authors } from '../services/authors';
 import { ActivatedRoute } from '@angular/router';
 import { map, switchMap } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
 import { MatCard } from '@angular/material/card';
 
 @Component({
     selector: 'app-author',
     templateUrl: './author.html',
     styleUrl: './author.css',
-    imports: [MatCard, AsyncPipe]
+    imports: [MatCard, AsyncPipe, NgOptimizedImage]
 })
 export class Author {
   private readonly authorService = inject(Authors);
--- src/app/post/post-detail/post-detail.ts
+++ src/app/post/post-detail/post-detail.ts
@@ -2,12 +2,13 @@ import { Component, input } from '@angular/core';
 import { PostData } from '../services/post';
 
 import { MatCard } from '@angular/material/card';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-    selector: 'app-post-detail',
-    templateUrl: './post-detail.html',
-    styleUrl: './post-detail.css',
-    imports: [MatCard]
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.html',
+  styleUrl: './post-detail.css',
+  imports: [MatCard, NgOptimizedImage]
 })
 export class PostDetail {
   protected readonly post = input.required<PostData | undefined>();
